6a26c5beb0899bc4cc9d7c778beda1025618bbf2
future-interactions <matt@future-interactions.com>
2022-06-13 20:04:13 +0100

    added tiles

--- sketch.js
+++ sketch.js
@@ -1,30 +1,43 @@
 let capture;
 let captureW;
 let captureH;
-let mosaicRes = 50;
+let numMosBlocks = 40;
 let mosaicWidth, mosaicHeight;
-let posX, posY;
+let posX = 60;
+let posY = 60;
+let col;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	capture = createCapture(VIDEO);
-	captureH = mosaicRes;
-	captureW = mosaicRes * 4 / 3;
+	captureH = numMosBlocks * .75;
+	captureW = numMosBlocks;
 	capture.size(captureW, captureH);
-	capture.hide();
+	//capture.hide();
+	console.log(captureW);
 
 }
 
 function draw() {
 	background(0);
-	drawMosaic(20, 20, 480, 480, 100);
+	drawMosaic(posX, posY, 480, 480, 100);
 }
 
 function drawMosaic(posX, posY, mosaicWidth, mosaicHeight, mosaicRes) {
 	if (capture.loadedmetadata) {
 		let c = capture.get(0, 0, captureH, captureH);
+
 		image(c, posX, posY, mosaicWidth, mosaicHeight);
+
+		for (let x = posX; x < mosaicWidth+posX; x += 12) {
+			for (let y = posY; y <  mosaicHeight+posY; y += 12) {
+				col = get(x, y);
+				//console.log(col);
+				fill(col);
+				noStroke();
+				rect(x, y, 10, 10);
+			}
+
+		}
 	}
 }
-
-//get(x, y, w, h)
