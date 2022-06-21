37c4de4bdaa2aeba390870c50f1c4e0b5a52612d
future-interactions <matt@future-interactions.com>
2022-06-21 09:31:47 +0100

    added hiRes image and lowRes image to retain camera res and create lowres sampler

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
-var video;
-
+let video;
+let hiResImg;
+let lowResImg;
+let poseNet;
+let pose;
 var vMosaicX = 48; //change this to change the length of a row of number of squares displayed in the mosaic
 var vMosaicY = vMosaicX * 0.75;
 var vW = 720; //desired width of mosaic to draw to screen
@@ -9,15 +12,13 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	pixelDensity(1);
 	video = createCapture(VIDEO);
-	video.size(vMosaicX, vMosaicY);
-	console.log("-----------------------------");
-	console.log("Mosaic width = " + vMosaicX);
-	console.log("Mosaic height = " + vMosaicY);
-	console.log("vScale = " + vScale);
-	console.log("desired width = " + vW);
-	console.log("width/2 = " + width / 2);
-	console.log("vW/2= " + vW / 2);
+	video.size(640, 480);
+	hiResImg = createGraphics(640, 480);
+	lowResImg = createGraphics(vMosaicX, vMosaicY);
+
 	video.hide();
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 	noSmooth();
 }
 
@@ -27,23 +28,29 @@ function draw() {
 	stroke(100);
 	noFill();
 	strokeWeight(1);
-	video.loadPixels();
+	hiResImg.copy(video, 0, 0, video.width, video.height, 0, 0, hiResImg.width, hiResImg.height);
+	if (pose) {
+		hiResImg.noStroke();
+		hiResImg.fill(255, 0, 0);
+		hiResImg.ellipse(pose.nose.x, pose.nose.y, 100, 100);
+		//hiResImg.updatePixels();
+		//ellipse(pose.nose.x * vScale - vScale - vScale, pose.nose.y * vScale - vScale - vScale, vScale * 10, vScale * 10)
+	}
+	lowResImg.copy(hiResImg, 0, 0, hiResImg.width, hiResImg.height, 0, 0, vMosaicX, vMosaicY);
 	noStroke();
 	fill(240, 240, 220);
 	rect(0 - vScale * 2, 0 - vScale * 2, vW * .75 + (vScale * 4), vW * .75 + (vScale * 4));
-
+	lowResImg.loadPixels();
 	noFill();
 	stroke(240, 240, 220);
 	rect(0 - vScale * 3, 0 - vScale * 3, vW * .75 + (vScale * 6), vW * .75 + (vScale * 6));
-
 	for (var y = 0; y < vMosaicY; y++) {
 		for (var x = 0; x < vMosaicY; x++) {
-			var index = (video.width - 1 - x + (y * video.width)) * 4;
-			var r = video.pixels[index + 0];
-			var g = video.pixels[index + 1];
-			var b = video.pixels[index + 2];
+			var index = (lowResImg.width -1 - x + (y  * lowResImg.width)) * 4;
+			var r = lowResImg.pixels[index + 0];
+			var g = lowResImg.pixels[index + 1];
+			var b = lowResImg.pixels[index + 2];
 			fill(r, g, b);
-			//	stroke(200, 200, 180);
 			rect(x * vScale, y * vScale, vScale, vScale);
 			noStroke();
 		}
@@ -54,5 +61,19 @@ function draw() {
 			noFill();
 			rect(x * vScale - vScale - vScale, y * vScale - vScale - vScale, vScale, vScale);
 		}
+	}
+
+
+	//image(hiResImg,-vMosaicX*vScale, 0); //show hiResImg as thumbnail video
+}
+
+function modelLoaded() {
+	//	console.log("model loaded");
+}
+
+function gotPoses(poses) {
+	//	console.log(poses);
+	if (poses.length > 0) {
+		pose = poses[0].pose;
 	}
 }
